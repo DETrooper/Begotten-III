89fa6b19588959ab2a35a68673b09a468d5adf23
Claudio Palacio <claudio.palacio@uabc.edu.mx>
2023-08-01 04:16:20 -0700

    RomanNumeralsAlgorithm
    improved roman numeral algorithms for faster processing

--- upload/gamemodes/cwbegotten/plugins/beliefs/plugin/derma/cl_belieftree.lua
+++ upload/gamemodes/cwbegotten/plugins/beliefs/plugin/derma/cl_belieftree.lua
@@ -35,64 +35,66 @@ local map = {
 local numbers = { 1, 5, 10, 50, 100, 500, 1000 }
 local chars = { "I", "V", "X", "L", "C", "D", "M" }
 
-local RomanNumerals = { }
+local RomanNumerals = {
+    map = {
+        [1000] = "M",
+        [900] = "CM",
+        [500] = "D",
+        [400] = "CD",
+        [100] = "C",
+        [90] = "XC",
+        [50] = "L",
+        [40] = "XL",
+        [10] = "X",
+        [9] = "IX",
+        [5] = "V",
+        [4] = "IV",
+        [1] = "I"
+    }
+}
 
 function RomanNumerals.ToRomanNumerals(s)
-    --s = tostring(s)
     s = tonumber(s)
-    if not s or s ~= s then error"Unable to convert to number" end
-    if s == math.huge then error"Unable to convert infinity" end
+    if not s or s ~= s then error "Unable to convert to number" end
+    if s == math.huge then error "Unable to convert infinity" end
     s = math.floor(s)
-    if s <= 0 then return s end
-	local ret = ""
-        for i = #numbers, 1, -1 do
-        local num = numbers[i]
-        while s - num >= 0 and s > 0 do
-            ret = ret .. chars[i]
+
+    local ret = ""
+    for num, char in pairs(RomanNumerals.map) do
+        while s >= num do
+            ret = ret .. char
             s = s - num
         end
-        --for j = i - 1, 1, -1 do
-        for j = 1, i - 1 do
-            local n2 = numbers[j]
-            if s - (num - n2) >= 0 and s < num and s > 0 and num - n2 ~= n2 then
-                ret = ret .. chars[j] .. chars[i]
-                s = s - (num - n2)
-                break
-            end
-        end
     end
+
     return ret
 end
 
 function RomanNumerals.ToNumber(s)
+    local map = {
+        I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000,
+    }
+
     s = s:upper()
     local ret = 0
-    local i = 1
-    while i <= s:len() do
-    --for i = 1, s:len() do
+    local prevValue = 0
+
+    for i = #s, 1, -1 do -- Traverse the string from right to left
         local c = s:sub(i, i)
-        if c ~= " " then -- allow spaces
-            local m = map[c] or error("Unknown Roman Numeral '" .. c .. "'")
-            
-            local next = s:sub(i + 1, i + 1)
-            local nextm = map[next]
-            
-            if next and nextm then
-                if nextm > m then 
-                -- if string[i] < string[i + 1] then result += string[i + 1] - string[i]
-                -- This is used instead of programming in IV = 4, IX = 9, etc, because it is
-                -- more flexible and possibly more efficient
-                    ret = ret + (nextm - m)
-                    i = i + 1
-                else
-                    ret = ret + m
-                end
-            else
-                ret = ret + m
-            end
+        local m = map[c]
+
+        if not m then
+            error("Unknown Roman Numeral '" .. c .. "'")
         end
-        i = i + 1
+
+        if m < prevValue then
+            ret = ret - m
+        else
+            ret = ret + m
+            prevValue = m
+        end
     end
+
     return ret
 end
 
