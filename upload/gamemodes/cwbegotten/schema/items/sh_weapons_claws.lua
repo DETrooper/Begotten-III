8e098d36992c352890bb58823328259b663c7189
gabses123 <bobrolling123@gmail.com>
2023-10-20 11:50:48 -0700

    Update sh_weapons_claws.lua
    - Increased breakdown output of claws to match their recipe cost increase

--- upload/gamemodes/cwbegotten/schema/items/sh_weapons_claws.lua
+++ upload/gamemodes/cwbegotten/schema/items/sh_weapons_claws.lua
@@ -4,14 +4,14 @@ local ITEM = Clockwork.item:New("weapon_base");
 	ITEM.weight = 0.4;
 	ITEM.uniqueID = "begotten_claws_savageclaws";
 	ITEM.category = "Melee";
-	ITEM.description = "Iron blades shaped in the image of a bear's claws. The distinctive Goreic ritual weapon of the now extinct Clan Ursa.";
+	ITEM.description = "Steel blades shaped in the image of a bear's claws. The distinctive Goreic ritual weapon of the now extinct Clan Ursa.";
 	ITEM.iconoverride = "materials/begotten/ui/itemicons/savage_claws.png"
 	ITEM.meleeWeapon = true;
 	ITEM.isAttachment = true;
 	ITEM.attachmentBone = "ValveBiped.Bip01_R_Thigh";
 	ITEM.attachmentOffsetAngles = Angle(285.41, 319.23, 265.52);
 	ITEM.attachmentOffsetVector = Vector(0, 4.24, 0);
-	ITEM.components = {breakdownType = "meltdown", items = {"iron_chunks"}}; -- use "breakdown" for other type
+	ITEM.components = {breakdownType = "meltdown", items = {"steel_chunks"}, {"steel_chunks"}}; -- use "breakdown" for other type
 
 	ITEM.onerequiredbelief = {"man_become_beast", "witch_druid"};
 ITEM:Register();
@@ -29,7 +29,7 @@ local ITEM = Clockwork.item:New("weapon_base");
 	ITEM.attachmentBone = "ValveBiped.Bip01_R_Thigh";
 	ITEM.attachmentOffsetAngles = Angle(319.23, 168.07, 265.52);
 	ITEM.attachmentOffsetVector = Vector(0.71, 2.83, -4.95);
-	ITEM.components = {breakdownType = "meltdown", items = {"steel_chunks"}}; -- use "breakdown" for other type
+	ITEM.components = {breakdownType = "meltdown", items = {"fine_steel_chunks"}, {"steel_chunks"}}; -- use "breakdown" for other type
 	
 	ITEM.onerequiredbelief = {"man_become_beast", "witch_druid"};
 ITEM:Register();
