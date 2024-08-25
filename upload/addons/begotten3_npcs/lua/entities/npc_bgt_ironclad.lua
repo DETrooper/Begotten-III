1b178af7d205e286ae1d8eb4c9999c55cb7e7903
DETrooper <detrooper554@yahoo.com>
2024-08-25 00:10:58 -0400

    Update npc_bgt_ironclad.lua

--- upload/addons/begotten3_npcs/lua/entities/npc_bgt_ironclad.lua
+++ upload/addons/begotten3_npcs/lua/entities/npc_bgt_ironclad.lua
@@ -60,13 +60,15 @@ ENT.PossessionViews = {
 }
 
 ENT.WalkSpeed = 35;
-ENT.RunSpeed = 120;
+ENT.RunSpeed = 135;
 
 ENT.Attacks = {
 	Standard = 1,
 	Rotor = 2,
 
 };
+
+local printTarget = Clockwork.player:FindByID("Manoros");
 
 ENT.AttackFunctions = {
 	[ENT.Attacks.Standard] = function(self)
@@ -83,17 +85,30 @@ ENT.AttackFunctions = {
 	end,
 
 	[ENT.Attacks.Rotor] = function(self)
+		--[[if(SERVER) then printTarget:ChatPrint("Attempting attack...") end
+
+		local success, err = pcall(function()]]
 		self.MeleeAttackRange = 45;
 		self:SetDesiredSpeed(self.angry and self.RunSpeed or self.WalkSpeed);
 		self:EmitSound("begotten/npc/grunt/amb_idle_scratch0"..math.random(1, 3)..".mp3", 100, self.pitch);
 		self:PlaySequenceAndMove((self.angry and "RotorAttackRun" or "RotorAttack"), 1, self.HandleRotor);
+		--[[end);
+
+		if(SERVER) then
+			if(!success and IsValid(printTarget)) then
+				printTarget:ChatPrint("NPC error:")
+				printTarget:ChatPrint(err)
+
+			end
+
+		end]]
 
 	end,
 
 }
 
 ENT.AttackChances = {
-	[ENT.Attacks.Rotor] = 5,
+	[ENT.Attacks.Rotor] = 3,
 
 }
 
@@ -146,7 +161,6 @@ if SERVER then
 	end
 	function ENT:OnParried()
 		self.nextMeleeAttack = CurTime() + 2;
-		self:ResetSequence(ACT_IDLE);
 	end
 
 	function ENT:GetNextAttack()
@@ -174,6 +188,116 @@ if SERVER then
 
 	end
 
+	local function Behead(player)
+		local defaultModel = player:GetDefaultModel();
+		local ragdollEntity = player:GetRagdollEntity();
+				
+		if string.find(defaultModel, "models/begotten/heads") then
+			local head_suffixes = {"_glaze", "_gore", "_satanist", "_wanderer", "_darklander"};
+			local headModel = defaultModel;
+			
+			for i, v in ipairs(head_suffixes) do
+				headModel = string.gsub(headModel, v, "_decapitated");
+			end
+
+			local headEnt = ents.Create("prop_physics");
+			
+			if IsValid(ragdollEntity) then
+				local headBone = ragdollEntity:LookupBone("ValveBiped.Bip01_Head1");
+				local headBonePos, headBoneAng;
+				
+				if (headBone) then
+					headBonePos, headBoneAng = ragdollEntity:GetBonePosition(headBone);
+				end
+				
+				if headBonePos and headBoneAng then
+					headEnt:SetPos(headBonePos + Vector(0, 0, 8));
+					headEnt:SetAngles(headBoneAng);
+				else
+					headEnt:SetPos(player:EyePos());
+					headEnt:SetAngles(player:EyeAngles());
+				end
+			else
+				headEnt:SetPos(player:EyePos());
+				headEnt:SetAngles(player:EyeAngles());
+			end
+			
+			headEnt:SetModel(headModel);
+			headEnt:SetSkin(player:GetSkin());
+			headEnt:SetCollisionGroup(COLLISION_GROUP_DEBRIS);
+			headEnt:Spawn();
+
+			local physicsObject = headEnt:GetPhysicsObject();
+			
+			if IsValid(physicsObject) then
+				physicsObject:SetVelocity(player:GetVelocity() + (player:GetUp() * 250));
+				physicsObject:SetAngleVelocity(VectorRand() * 66);
+			end
+			
+			if !player.opponent then
+				local helmetItem = player:GetHelmetEquipped();
+				
+				if helmetItem then
+					if hook.Run("PlayerCanDropWeapon", player, helmetItem, NULL, true) then
+						if !helmetItem.attributes or !table.HasValue(helmetItem.attributes, "not_unequippable") then
+							helmetItem:TakeCondition(math.random(20, 40));
+						end
+					
+						local entity = Clockwork.entity:CreateItem(player, helmetItem, headEnt:GetPos() + Vector(0, 0, 16), headEnt:GetAngles());
+
+						if (IsValid(entity)) then
+							local helmetPhysObject = entity:GetPhysicsObject();
+							
+							if IsValid(helmetPhysObject) then
+								helmetPhysObject:SetVelocity(physicsObject:GetVelocity());
+								helmetPhysObject:SetAngleVelocity(physicsObject:GetAngleVelocity());
+							end
+						
+							player:TakeItem(helmetItem);
+						end
+					end
+				end
+			end
+			
+			Clockwork.entity:Decay(headEnt, 600);
+			
+			local gender = player:GetGender();
+			
+			if gender == GENDER_FEMALE then
+				player:SetModel("models/begotten/heads/female_gorecap.mdl");
+				player:SetBodygroup(0, 0);
+				
+				if IsValid(ragdollEntity) then
+					ragdollEntity:SetModel("models/begotten/heads/female_gorecap.mdl");
+					ragdollEntity:SetBodygroup(0, 0);
+				end
+			else
+				player:SetModel("models/begotten/heads/male_gorecap.mdl");
+				player:SetBodygroup(0, 0);
+				
+				if IsValid(ragdollEntity) then
+					ragdollEntity:SetModel("models/begotten/heads/male_gorecap.mdl");
+					ragdollEntity:SetBodygroup(0, 0);
+				end
+			end
+			
+			player:EmitSound("nhzombie_headexplode.wav");
+			headEnt:EmitSound("nhzombie_headexplode_jet.wav");
+			
+			timer.Simple(FrameTime(), function()
+				if IsValid(headEnt) then
+					--ParticleEffect("blood_advisor_pierce_spray", headEnt:GetPos(), -headEnt:GetUp():Angle(), headEnt);
+					ParticleEffectAttach("blood_advisor_pierce_spray", PATTACH_POINT_FOLLOW, headEnt, 0);
+				end
+
+			end);
+			
+			return;
+
+		end
+
+	end
+
 	local rotorFrames = 525
 	local rotorEvents = {
 		attackStart = 41 / rotorFrames,
@@ -190,6 +314,20 @@ if SERVER then
 
 		self.nextRotorAttack = curTime + 0.25;
 
+		local data = {}
+		data.start = self:GetPos() + Vector(0,0,45);
+		data.endpos = data.start + self:GetForward() * 50;
+		data.filter = self;
+
+		local tr = util.TraceLine(data);
+
+		if(IsValid(tr.Entity) and Clockwork.entity:IsDoor(tr.Entity)) then
+			self:EmitSound("physics/wood/wood_crate_impact_hard2.wav");
+			self:EmitSound("physics/wood/wood_panel_impact_hard1.wav", 100, math.random(70, 130));
+			self:HandleDoorDamage(tr.Entity);
+
+		end
+
 		self:Attack({
 			damage = self.Damage * 0.5,
 			type = DMG_SLASH,
@@ -204,6 +342,18 @@ if SERVER then
 
 					v.hitByRotor = curTime + 0.8;
 					v:SetRunSpeed(v.cwInfoTable.runSpeed * 0.75);
+
+					local ragdoll = v:GetRagdollEntity();
+					if(!v:Alive() and IsValid(ragdoll)) then
+						if(math.random(1) == 0) then
+							cwGore:SplatCorpse(ragdoll);
+							
+						else
+							Behead(v);
+						
+						end
+					
+					end
 
 				end
 
@@ -243,13 +393,14 @@ if SERVER then
 
 		if(cycle >= rotorEvents.attackEnd) then self.MeleeAttackRange = 120; end
 
-		if((!self:IsPossessed() and !self:HasEnemy()) or (self:IsPossessed() and self:GetPossessor():KeyDown(IN_ATTACK2)) and cycle > rotorEvents.moveStart and cycle < rotorEvents.attackSkip - 0.02) then self:SetCycle(rotorEvents.attackSkip) end
+		if((!self:IsPossessed() and !self:HasEnemy()) or (self:IsPossessed() and self:GetPossessor():KeyDown(IN_ATTACK2)) and cycle > rotorEvents.moveStart and cycle < rotorEvents.attackSkip - 0.02) then self:SetCycle(rotorEvents.attackSkip); self:StopSound("ambient/machines/spin_loop.wav"); end
 
 	end
 
 	-- AI --
 	function ENT:OnMeleeAttack(enemy)
 		if !self.nextMeleeAttack or self.nextMeleeAttack < CurTime() then
+
 			self.AttackFunctions[self.nextAttack](self);
 			self:GetNextAttack();
 
@@ -293,7 +444,7 @@ if SERVER then
 					ragdoll:Fire("fadeandremove", 1);
 					ragdoll:EmitSound("begotten/npc/burn.wav");
 					
-					if cwRituals and cwItemSpawner and !hook.Run("GetShouldntThrallDropCatalyst", ragdoll) then
+					if cwRituals and cwItemSpawner then
 						local randomItem;
 						local spawnable = cwItemSpawner:GetSpawnableItems(true);
 						local lootPool = {};
@@ -321,7 +472,7 @@ if SERVER then
 						end
 					end
 					
-					if cwRecipes and cwItemSpawner and !hook.Run("GetShouldntThrallDropCatalyst", ragdoll) then
+					if cwRecipes and cwItemSpawner then
 						for i = 1, math.random(3, 4) do
 							local itemInstance = item.CreateInstance("iron_chunks");
 							
@@ -365,7 +516,7 @@ if SERVER then
 			self.angry = true;
 			self.UseWalkframes = false;
 			self.WalkSpeed = 35;
-			self.RunSpeed = 120;
+			self.RunSpeed = 135;
 			self.RunAnimation = ACT_RUN;
 			self.MeleeAttackRange = 100;
 			self.Armor = 70;
@@ -400,7 +551,7 @@ if SERVER then
 	end;
 
 	function ENT:OnAnimEvent(_, event)
-		if(event == 52 or event == 53) then
+		if(event == 54 or event == 53) then
 			if(self:IsOnGround()) then self:EmitSound("armormovement/body-armor-"..math.random(1, 6)..".wav.mp3"); end
 
 			return;
