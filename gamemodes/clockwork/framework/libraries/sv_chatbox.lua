ba21e8a53058bea09795a2b23afb7d26d1473379
thinedave <94513260+thinedave@users.noreply.github.com>
2026-01-30 20:55:53 -0800

    AddActionInRadius function

--- gamemodes/clockwork/framework/libraries/sv_chatbox.lua
+++ gamemodes/clockwork/framework/libraries/sv_chatbox.lua
@@ -136,3 +136,16 @@ end;
 function Clockwork.chatBox:SetMultiplier(multiplier)
 	self.multiplier = multiplier;
 end;
+
+function Schema:AddActionInRadius(pos, radius, text)
+    local listeners = {};
+    for _, v in _player.Iterator() do
+		if(!v:HasInitialized()) then continue; end
+		if(pos:DistToSqr(v:GetPos()) > (radius * radius)) then continue; end
+
+		listeners[#listeners + 1] = v
+
+	end
+
+    Clockwork.chatBox:Add(listeners, nil, "itnofake", text);
+end
